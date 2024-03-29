6b594b70fed4c1cec5ce7d03360e77922e0250a5
kh park <pkgz888@gmail.com>
2024-03-29 09:36:28 +0900

    create addressable simple cube when you press a button

---
+++ Assets/01.Scripts/AddressableLoad.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+public class AddressableLoad : MonoBehaviour
+{
+
+    public void CreateObj()
+    {
+        Addressables.InstantiateAsync("Cube", new Vector3(0,0,0), Quaternion.identity); //Adressable의 이름과 위치
+        //첫번쨰 인자는 키값인데 name, 위치, 사원수값의 회전값
+        //Quaternion.identity - 그 객체가 가지고 있는 기본 회전값
+    }
+
+
+}
