58dd90835f9879dad85511414807d19fdd9d09fc
kh park <pkgz888@gmail.com>
2024-03-29 09:57:50 +0900

    build addressable, and add Naming to Cubes

--- Assets/01.Scripts/AddressableLoad.cs
+++ Assets/01.Scripts/AddressableLoad.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 using UnityEngine.AddressableAssets;
 public class AddressableLoad : MonoBehaviour
 {
+    int index;
 
     public void CreateObj()
     {
         Addressables.InstantiateAsync("Cube", new Vector3(0,0,0), Quaternion.identity); //Adressable의 이름과 위치
         //첫번쨰 인자는 키값인데 name, 위치, 사원수값의 회전값
         //Quaternion.identity - 그 객체가 가지고 있는 기본 회전값
+
+        GameObject.Find("Cube(Clone").name = "Cube" + index;
+        index++;
     }
 
 
